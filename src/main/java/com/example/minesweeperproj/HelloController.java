9b87d2f4752a035698302955e9f0ec831dfd53f9
max <maxwoess1105@gmail.com>
2024-01-17 10:41:30 +0100

    Added basic grid pane in fxml

--- src/main/java/com/example/minesweeperproj/HelloController.java
+++ src/main/java/com/example/minesweeperproj/HelloController.java
@@ -1,20 +1,6 @@
 package com.example.minesweeperproj;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-    }
-
-
-
-
-
 
 
 }
