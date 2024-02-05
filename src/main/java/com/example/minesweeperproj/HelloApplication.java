3483e118db856d4ce13ca0d61dd273455a9b078f
max <maxwoess1105@gmail.com>
2024-02-05 16:01:33 +0100

    added simple timer

--- src/main/java/com/example/minesweeperproj/HelloApplication.java
+++ src/main/java/com/example/minesweeperproj/HelloApplication.java
@@ -1,18 +1,31 @@
 package com.example.minesweeperproj;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.stage.Popup;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
 
+    private int secondsElapsed = 0;
+    private Timeline timer;
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException{
+        timer = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            secondsElapsed++;
+            stage.setTitle(String.valueOf(Converter(secondsElapsed)));
+        }));
+        timer.setCycleCount(Timeline.INDEFINITE);
+        timer.play();
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         int[] difficulty123 = HelloController.setDifficult;
         System.out.println("nach");
@@ -21,11 +34,25 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         stage.setResizable(false);
-
     }
+    static String Converter(int seconds){
 
+        int sec = seconds;
+        int min = 0;
+        int hour = 0;
+        String myString;
+        while (sec >= 60){
+            min++;
+            sec = sec - 60;
+        }
+        while (min >= 60){
+            hour++;
+            min = min - 60;
+        }
+        myString = hour + ":" + min + ":" + sec;
+        return myString;
+    }
     public static void main(String[] args) {
         launch();
-
     }
 }
